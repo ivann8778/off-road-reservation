340dee42846cba249b48bb7ba903fdc0eec97363
IvannPopov <ivannpopov11@gmail.com>
2024-10-18 10:14:55 +0300

    project start

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createPinia } from 'pinia'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+createApp(App).use(router).use(createPinia()).mount('#app')
+
+import 'bootstrap/dist/js/bootstrap.js'
